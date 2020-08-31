a526ec39628abbdac6012a53276dfdfc9bc4cc67
rawah <rawah@pdx.edu>
2020-08-30 22:15:19 -0700

    clean up process done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      TWITTER CLONE!
+    </div>
+  );
+}
+
+export default App;
